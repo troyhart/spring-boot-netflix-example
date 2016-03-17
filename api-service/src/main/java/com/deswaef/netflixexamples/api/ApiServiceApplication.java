dd245d54d004ebd27f30aa94dcc20160cac1710f
Quinten De Swaef <quintendeswaef@gmail.com>
2016-03-17 23:42:44 +0100

    creating our new service, the api layer

---
+++ api-service/src/main/java/com/deswaef/netflixexamples/api/ApiServiceApplication.java
@@ -0,0 +1,20 @@
+package com.deswaef.netflixexamples.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class ApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(ApiServiceApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
+    }
+
+}
