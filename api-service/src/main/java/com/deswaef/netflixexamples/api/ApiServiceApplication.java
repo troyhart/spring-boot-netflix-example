409a13c5da2a7cfee68be709218afdaa94d17917
Quinten De Swaef <quintendeswaef@gmail.com>
2016-03-29 12:34:49 +0200

    Enabling Feign Clients

--- api-service/src/main/java/com/deswaef/netflixexamples/api/ApiServiceApplication.java
+++ api-service/src/main/java/com/deswaef/netflixexamples/api/ApiServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableFeignClients
 public class ApiServiceApplication {
 
     public static void main(String[] args) {
