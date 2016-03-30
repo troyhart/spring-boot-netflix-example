2a01b39a12c8cc5077dd3e5273b44825ab19d6b6
Quinten De Swaef <quintendeswaef@gmail.com>
2016-03-30 12:39:17 +0200

    name was not yet available in 1.0.7.RELEASE

--- api-service/src/main/java/com/deswaef/netflixexamples/api/reddit/client/RedditResource.java
+++ api-service/src/main/java/com/deswaef/netflixexamples/api/reddit/client/RedditResource.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "reddit", url = "${com.deswaef.reddit.url}")
+@FeignClient(url = "${com.deswaef.reddit.url}")
 public interface RedditResource {
 
     @RequestMapping(method = RequestMethod.GET, value = "/java.json")
