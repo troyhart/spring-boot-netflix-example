7359e658526d0df539eba82f7cb1e2925fe5432c
Quinten De Swaef <quintendeswaef@gmail.com>
2016-03-30 12:26:12 +0200

    Upgrade to RC1 Remove HystrixObservable creation and used real Rx.Observer Changed from DiscoveryClient to EurekaClient, because Netflix added an interface to the DiscoveryClient, to make the transition to v2 easier. This results in a failure when trying to inject DiscoveryClient, which is a subclas now

--- api-service/src/main/java/com/deswaef/netflixexamples/api/ApiServiceApplication.java
+++ api-service/src/main/java/com/deswaef/netflixexamples/api/ApiServiceApplication.java
@@ -5,13 +5,14 @@ import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableCircuitBreaker
 @EnableHystrixDashboard
 @EnableFeignClients
