15d07a541f34baf3f43189fb25f6204a4d641ce5
Quinten De Swaef <quintendeswaef@gmail.com>
2016-03-31 12:54:58 +0200

    Added configuration class

--- api-service/src/main/java/com/deswaef/netflixexamples/api/reddit/client/RedditResource.java
+++ api-service/src/main/java/com/deswaef/netflixexamples/api/reddit/client/RedditResource.java
@@ -1,11 +1,12 @@
 package com.deswaef.netflixexamples.api.reddit.client;
 
 import com.deswaef.netflixexamples.api.reddit.model.RedditResponse;
+import configuration.feign.RedditFeignConfiguration;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "reddit-service", url = "${com.deswaef.reddit.url}")
+@FeignClient(name = "reddit-service", url = "${com.deswaef.reddit.url}", configuration = RedditFeignConfiguration.class)
 public interface RedditResource {
 
     @RequestMapping(method = RequestMethod.GET, value = "/java.json")
